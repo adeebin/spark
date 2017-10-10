f39e6d5d2b8c02960615ca03c6e2957b21b92991
Adeeb <adeeb.in@gmail.com>
2017-10-10 15:54:44 +0530

    G001: spark-java project created for git demo

---
+++ spark-java/src/com/env/spark/git/Hello.java
@@ -0,0 +1,17 @@
+package com.env.spark.git;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		printMessage();
+	}
+	
+	public static void printMessage() {
+		System.out.println("Hello Git !");
+	}
+	
+	public static void printMessage(String msg) {
+		System.out.println(msg);
+	}
+
+}
